99e277c524a0e8d07df627f025d0c42c45ac4d14
cheney <windchen@163.com>
2024-07-07 17:28:41 +0800

    三绕组温度电阻数据上传需要确认

---
+++ ABBDataManagerSystem/Pages/TempRiseResistanceDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ABBDataManagerSystem.Pages
+{
+    /// <summary>
+    /// TempRiseResistanceDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class TempRiseResistanceDialog : Window
+    {
+        public TempRiseResistanceDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btConfirm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
