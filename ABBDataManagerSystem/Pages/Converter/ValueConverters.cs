ef79c064ce988ce40e06c2136f241769f944550e
chenchangjian <chenchangjian@huawei.com>
2024-07-24 12:33:06 +0800

    温升电阻数据格式化

---
+++ ABBDataManagerSystem/Pages/Converter/ValueConverters.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ABBDataManagerSystem.Pages.Converter
+{
+    public class DateFormatter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("MM/dd/yyyy");
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NumberFormatter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is float number)
+            {
+                return Utils.FormatFloat(number, 4);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
